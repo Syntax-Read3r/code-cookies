63fd355b72dad16da6e238aed2487b8006357918
C Munyaradzi Chikara <SyntaxRead3r@gmail.com>
2022-05-18 16:30:30 +0100

    Add script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 'use scrict'
+let names = prompt('What is your name?');
+let check = confirm('Would you like a cookie?') 
 
+if(check === false) {
+    
+    alert('Please close the browser, thank you for visiting code:cookies')
+} 
+
+let cookie = prompt('What is your favorite ice-creame between vanilla, chocolate and strawberry')
+
+
+
+if (cookie == 'vanilla') {
+    document.write(`Hello, ${names}. Your bag full of ${cookie} cookies is on it's way.`)
+} else if (cookie == 'chocolate') {
+    document.write(`Hello, ${names}. Your bag full of ${cookie} cookies is on it's way.`)
+} else if (cookie == 'strawberry' || cookie == 'strawberries') {
+    document.write(`Hello, ${names}. Your bag full of ${cookie} cookies is on it's way.`)
+} else {
+    document.write(`Cookie monster is coming to get you!! Muhahaha`)
+}
