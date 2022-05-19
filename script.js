36424aaa2c27431a22e85e55ec3e1517fa91648b
C Munyaradzi Chikara <SyntaxRead3r@gmail.com>
2022-05-19 16:26:53 +0100

    Add image,(cm.pg) and js.script update

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ if (cookie == 'vanilla') {
     document.write(`Cookie monster is coming to get you!! Muhahaha`)
 }
 }
+
+function getRating() {
+    let res = '';
+    let rate = prompt('Rate your love for cookies 1-5');
+
+    while (rate > 5 || rate < 1) {
+        rate = prompt('Please enter a value between 1 and 5')
+    };
+
+    for(let x = 1; x< rate; x++) {
+        res += '<img class="cookie" src="cm.png"/>'
+    }
+    return document.write(res)
+}
